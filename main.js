c64cb51fcbf6eb3a9ed1072c2d7c4591b6187b0d
Adam Bruzon <adam.bruzon@centrica.com>
2020-02-18 17:47:32 +0000

    Set up, not setup

--- main.js
+++ main.js
@@ -6,29 +6,6 @@ const hl = urlParams.get('hl');
 var firstDate, option;
 var highlighter = "";
 
-var faqs = [
-  {
-    heading: `<h4 slot="heading">Why isn't my plan 12 months?</h4>`,
-    content: `<p>All plans in your area end on the same month so its easier for our meter readers to do their job. The duration of your plan will depend on how far you are from that month.</p>`
-  },
-  {
-    heading: `<h4 slot="heading">How did you calculate my monthly payment?</h4>`,
-    content: `<p>We use everything we know about your energy consumption to estimate your average usage and charge you a fixed amount every month.</p>`
-  },
-  {
-    heading: `<h4 slot="heading">How could my credit affect my future payments?</h4>`,
-    content: `<p>If you’re in credit when you set up a Direct Debit, we’ll offer you two options: </p><ol class="ol-numbered"><li>Take the credit as a refund cheque</li><li>Deduct your credit from the expected cost of your energy</li></ol><p>Taking a refund cheque means we send you the money you have overpaid. To calculate your monthly payments, we divide the expected cost of your energy by the number of months in your plan.</p><p>The second option means that we use the money you have overpaid to reduce your monthly payments over the next 6-12 months. We deduct the credit from your expected energy cost, then divide this reduced amount by the number of months in your plan. This makes your monthly payments smaller.</p><p>It depends on whether you want the money immediately, or you would like to reduce your payments for the length of your plan. One of the potential downsides of reducing your payments with your credit, is that they may increase next year – as there should be no credit to reduce them next time.</p>`
-  },
-  {
-    heading: `<h4 slot="heading">When can I expect my refund?</h4>`,
-    content: `<p>Your refund will be processed once you have set up your Direct Debit. This is issued to you in the form of a cheque, and will be with you in the next 10 working days.</p>`
-  },
-  {
-    heading: `<h4 slot="heading">Why are accounts reviewed?</h4>`,
-    content: `<p>All plans are reviewed on a periodic basis to ensure that you are paying the right amount based on your energy consumption.</p>`
-  }
-];
-
 if (date > moment.utc().date()) { firstDate = moment.utc().date(date).format("Do MMMM YYYY"); }
 else { firstDate = moment.utc().date(date).add(1, "months").format("Do MMMM YYYY"); }
 
